e66dffec45c3438510ee2d26ad7f5f4fd370b4be
Vipin Tiwar <vipinvipin1997vipin@gmail.com>
2025-10-30 22:37:29 +0530

    some fixes

--- backend/app/controllers/users_controller.ts
+++ backend/app/controllers/users_controller.ts
@@ -1,13 +1,35 @@
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class UsersController {
-  user = ({ auth }: HttpContext) => {
-    return auth.user
+  user = ({ auth, response }: HttpContext) => {
+    try {
+      const user = auth.user
+
+      return response.ok({
+        success: true,
+        user,
+      })
+    } catch (error) {
+      return response.internalServerError({
+        success: false,
+        message: 'Something went wrong while fetching user.',
+      })
+    }
   }
 
-  userDashboard = async ({ auth }: HttpContext) => {
-    const user = auth.user
-    const projects = await user?.related('projects').query()
-    return projects
+  userDashboard = async ({ auth, response }: HttpContext) => {
+    try {
+      const user = auth.user
+      const projects = await user?.related('projects').query()
+      return response.ok({
+        success: true,
+        projects,
+      })
+    } catch (error) {
+      return response.internalServerError({
+        success: false,
+        message: 'Something went wrong while fetching user.',
+      })
+    }
   }
 }
